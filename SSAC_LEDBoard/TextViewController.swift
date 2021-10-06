7837d172657e90ba71f2129be955421e50390424
yoogail105 <yoogail105@gmail.com>
2021-10-06 16:45:52 +0900

    TEST

---
+++ SSAC_LEDBoard/TextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TextViewController.swift
+//  SSAC_LEDBoard
+//
+//  Created by minjoohehe on 2021/10/06.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("TEST")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
