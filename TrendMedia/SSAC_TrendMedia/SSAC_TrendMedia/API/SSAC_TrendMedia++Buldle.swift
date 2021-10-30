0340f74d2781342c570132954195285229572c88
yoogail105 <yoogail105@gmail.com>
2021-10-30 15:41:58 +0900

    hide api key

--- TrendMedia/SSAC_TrendMedia/SSAC_TrendMedia/API/SSAC_TrendMedia++Buldle.swift
+++ TrendMedia/SSAC_TrendMedia/SSAC_TrendMedia/API/SSAC_TrendMedia++Buldle.swift
@@ -10,21 +10,21 @@ import Foundation
 import Foundation
 
 extension Bundle {
-
+    
+    
+    var NAVER_ID: String {
+        guard let file = self.path(forResource: "NaverMovieInfo", ofType: "plist") else {return ""}
+        guard let resource  = NSDictionary(contentsOfFile: file) else { return""}
+        guard let key = resource["NAVER_ID"] as? String else { fatalError("NaverMovieInfo.plist에 API_KEY 설정을 해주세요.")}
+        return key
+    }
     
     var NAVER_SECRET: String {
-        
-        var NAVER_SECRET: String {
-            guard let file = self.path(forResource: "NaverMovieInfo", ofType: "plist") else {return ""}
-            guard let resource  = NSDictionary(contentsOfFile: file) else { return""}
-            guard let key = resource["NAVER_SECRET_KEY"] as? String else { fatalError("NaverMovieInfo.plist에 API_KEY 설정을 해주세요.")}
-            return key
-            
         guard let file = self.path(forResource: "NaverMovieInfo", ofType: "plist") else {return ""}
         guard let resource  = NSDictionary(contentsOfFile: file) else { return""}
         guard let key = resource["NAVER_SECRET_KEY"] as? String else { fatalError("NaverMovieInfo.plist에 API_KEY 설정을 해주세요.")}
         return key
-    
+        
     }
+    
 }
-
